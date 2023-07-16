a347b09de21dad85982211f82b0070e4b3e5b0e7
rehhana <ashiqtalmustahiil@gmail.com>
2023-07-16 00:12:45 -0700

    answera of grade

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const grade=78
+if(grade<= 100 && grade >=90){
+    console.log('You got an A grade')
+}else if(grade<=89 && grade>=75){
+    console.log('You got an B grade')
+}else if(grade<=74 && grade>=60){
+    console.log('You got c grade')
+}else{
+    console.log('You got an FAILED')
+
+}
